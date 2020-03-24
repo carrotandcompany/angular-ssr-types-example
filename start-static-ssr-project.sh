6b921ff19cbf2bca3bc739813091f8c7f504bec3
askoki <arian.skoki@cnc.io>
2020-03-24 11:05:04 +0100

    Add script for starting static ssr project

---
+++ start-static-ssr-project.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+DOCKER_STATIC_SSR_SERVER="static-ssr-server"
+
+# build server for static ssr:
+docker build -f conf/static/Dockerfile --target static-ssr-server -t $DOCKER_STATIC_SSR_SERVER .
+
+docker container rm -f $DOCKER_STATIC_SSR_SERVER || true
+
+docker container run --name $DOCKER_STATIC_SSR_SERVER -p 8080:8080 -d $DOCKER_STATIC_SSR_SERVER
